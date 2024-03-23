48935f4c76c109e0663b2a9629008ce6939cf5c1
Gabriel Camurça <projetosdocamurca@gmail.com>
2024-03-22 22:32:57 -0300

    Implements healthcheck route

--- adapter/http/main.go
+++ adapter/http/main.go
@@ -3,12 +3,15 @@ package http
 import (
 	"net/http"
 
+	"github.com/gabrielcamurcab/planejador-financeiro-go/adapter/http/actuator"
 	"github.com/gabrielcamurcab/planejador-financeiro-go/adapter/http/transaction"
 )
 
 func Init() {
 	http.HandleFunc("/transactions", transaction.GetTransactions)
 	http.HandleFunc("/transactions/create", transaction.CreateATransaction)
+
+	http.HandleFunc("/health", actuator.Health)
 
 	http.ListenAndServe(":8080", nil)
 }
